a367f93aa674bc9ef6ef20c101a67ae3035a3c90
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2022-11-29 17:16:45 +0300

    update: header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,24 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
+import Input from "@/components/UI/Input/index";
 
 const Header: React.FC = () => {
-  return <div className="container mx-auto bg-red-500">test</div>;
+  return (
+    <div className="container mt-5 flex flex-col items-center justify-between px-2 md:mx-auto md:flex-row">
+      <div className="mb-6 flex flex-col items-center md:mb-0 md:flex-row">
+        <div>
+          <h1 className="text-4xl font-bold tracking-wider text-white">
+            Anonime
+          </h1>
+        </div>
+        <nav className="space-x-4 md:ml-24">
+          <Link to="/">Home</Link>
+          <Link to="/latest">List anime</Link>
+        </nav>
+      </div>
+      <Input placeholder="Search anime or movie" />
+    </div>
+  );
 };
 
 export default Header;

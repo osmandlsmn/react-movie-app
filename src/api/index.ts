4e3d2edf17e8ac9d3b27e9066949e267fcaca01f
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2022-11-29 18:45:35 +0300

    chore: update axios configuration

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: import.meta.env.VITE_MOVIE_API_URL,
-  params: { api_key: "9eb158a047cd680a2d3fc9aba7c6f4f7" },
+  params: { api_key: import.meta.env.VITE_MOVIE_API_KEY },
 });

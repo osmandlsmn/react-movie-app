649e45d49d71a6df00b6ce87672001cba3d57b7c
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2022-11-29 18:45:00 +0300

    style: add font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,8 +3,13 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: "Poppins",
+      },
       colors: {
         background: "#192026",
+        primary: "#868686",
+        secondary: "#374151",
       },
     },
   },

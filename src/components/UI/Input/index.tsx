b41bee8da01e4b18a19f3d8e15e4a25474194348
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2022-11-30 17:22:37 +0300

    feat: update input component

--- src/components/UI/Input/index.tsx
+++ src/components/UI/Input/index.tsx
@@ -6,9 +6,9 @@ interface InputProps {
 
 const Input: React.FC<InputProps> = ({ placeholder }) => {
   return (
-    <div className="flex h-10 w-[370px] items-center rounded-2xl bg-secondary px-5 text-lg">
+    <div className="text-md flex h-10 w-[370px] items-center rounded-2xl bg-secondary px-5">
       <input
-        className="bg-transparent outline-none placeholder:tracking-wider "
+        className="w-full bg-transparent outline-none placeholder:tracking-wider "
         placeholder={placeholder}
       />
     </div>

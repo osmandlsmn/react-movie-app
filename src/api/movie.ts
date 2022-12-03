25a24430f8273802a114114db7e6578f4cbe5e55
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2022-12-03 16:47:36 +0300

    feat: add search api

--- src/api/movie.ts
+++ src/api/movie.ts
@@ -1,4 +1,6 @@
-import type { TrendResponse } from "@/types";
+import type { TrendResponse, SearchResponse } from "@/types";
 import axios from "./index";
 
 export const getTrends = () => axios.get<TrendResponse>("/trending/movie/week");
+
+export const searchMovie = (searchValue: string) => axios.get<SearchResponse>(`/search/movie?query=${searchValue}`);

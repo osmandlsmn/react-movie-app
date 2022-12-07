1f7a08ca0fbe903f387ab591e55144bc0445f057
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2022-12-07 03:20:46 +0300

    feat: add moviecard animation

--- src/containers/Home/MovieCard.tsx
+++ src/containers/Home/MovieCard.tsx
@@ -9,7 +9,13 @@ interface CardProps {
 const MovieCard: React.FC<CardProps> = ({ movie }) => {
   return (
     <div className="flex flex-col  rounded-lg bg-[#050E12]">
-      <img className="h-[240px] w-full rounded-lg" src={getPhotoURL(BackdropSizes.w300, movie.backdrop_path)} alt="" />
+      <div className="h-min overflow-hidden rounded-md">
+        <img
+          className="h-[240px] w-full cursor-pointer rounded-lg transition-all duration-500 hover:scale-125"
+          src={getPhotoURL(BackdropSizes.w300, movie.backdrop_path)}
+          alt=""
+        />
+      </div>
       <div className="p-2">
         <h3 className="truncate text-lg font-semibold text-white ">{movie.title}</h3>
         <p>{movie.release_date.toString()}</p>
